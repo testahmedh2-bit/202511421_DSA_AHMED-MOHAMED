90f5b59282e15f421ff6d24ab6e4e2ac4a6bbf68
testahmedh2-bit <testahmedh2@gmail.com>
2026-05-13 20:44:08 +0200

    Update assignment.cpp

--- assignment.cpp
+++ assignment.cpp
@@ -4,25 +4,21 @@
 using namespace std;
 
 int main() {
-    // 1. Chosen number
+    
     int chosenNumber = 7;
 
-    // 2. Variable for user input
+    
     int userGuess;
 
-    // 3. Vector to store all attempts
     vector<int> attempts;
 
     cout << "Guess the chosen number: ";
 
-    // 4. While loop starts
     while (true) {
         cin >> userGuess;
 
-        // Save the value into the list
         attempts.push_back(userGuess);
 
-        // 5. Comparison logic
         if (userGuess > chosenNumber) {
             cout << "Too high" << endl;
             cout << "Try again: ";
@@ -34,8 +30,7 @@ int main() {
             break;
         }
     }
-
-    // 6. Display all stored guesses
+    
     cout << "\nYour history of guesses: " << endl;
     for (int i = 0; i < attempts.size(); i++) {
         cout << attempts[i] << " ";
